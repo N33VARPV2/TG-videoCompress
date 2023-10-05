8db008500bbdc89d7c052bafcb29d7d38c9a8d51
N33VARP <118649748+N33VARPV2@users.noreply.github.com>
2023-10-05 15:49:47 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -19,7 +19,7 @@ try:
     APP_ID = "11657778" config("APP_ID")
     API_HASH = "56b209fe074993cf4d123fdfb74ab545" config("API_HASH")
     BOT_TOKEN = "6690933985:AAGoQE5HQAHqOBkZ4K03FQzAxTGbX2JHKow" config("BOT_TOKEN")
-    DEV = 5383602320
+    DEV = "5383602320"
     OWNER = "5383602320" config("OWNER")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMB = config("THUMBNAIL")
